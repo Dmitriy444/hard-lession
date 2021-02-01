5f40442df68c5088c3573659224fed65c3a3deb1
Dmitri Larin <Diman-Larin@mail.ru>
2021-02-01 13:34:05 +0400

    Исправил усложнённое дз и удалил комментарий

--- script/script2.js
+++ script/script2.js
@@ -17,36 +17,3 @@ console.log(c);
 console.log(r);
 console.log(typeof(str1));
 console.log(n);
-
-
-
-
-
-/*let i = ('' + num).split('');
-
-let c = i.reduce(function(a, b) {
-    return a * b
-});
-
-let r = c ** 3;
-let arr = ('' + r).split('');
-
-
-console.log(i);
-console.log(c);
-//console.log(c ** 3);
-
-console.log(r);
-console.log(arr);
-console.log(arr[0], arr[1]);
-*/
-
-
-
-
-
-
-
-
-
-//console.log(num.charAt(0));
