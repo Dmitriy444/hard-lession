98a1c81c435f5566830a91a05cf181acba0c2601
Dmitri Larin <Diman-Larin@mail.ru>
2021-02-01 13:33:00 +0400

    Исправил усложнённое дз

--- script/script2.js
+++ script/script2.js
@@ -2,8 +2,27 @@
 
 let num = 266219;
 
+let str = String(num);
+let i = ('' + str).split('');
 
-let i = ('' + num).split('');
+let c = i.reduce(function(a, b) {
+    return a * b
+});
+let r = c ** 3;
+let str1 = String(r);
+let n = Number(str1.substring(0, 2));
+
+console.log(str);
+console.log(c);
+console.log(r);
+console.log(typeof(str1));
+console.log(n);
+
+
+
+
+
+/*let i = ('' + num).split('');
 
 let c = i.reduce(function(a, b) {
     return a * b
@@ -20,7 +39,7 @@ console.log(c);
 console.log(r);
 console.log(arr);
 console.log(arr[0], arr[1]);
-
+*/
 
 
 
